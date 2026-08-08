8d69173d16a20a126e962a0f9a49a08ad3f68e55
murilovanzan <murilovanzandasilva@gmail.com>
2026-08-08 05:24:48 -0300

    Atualização do .sql e diagrama ER gerados pelo 'erd editor' e adição do .sql gerado pelo phpmyadmin.

--- Banco de dados/loja-simples - sql gerado por erd editor.sql
+++ Banco de dados/loja-simples - sql gerado por erd editor.sql
@@ -3,16 +3,16 @@ CREATE TABLE compra
 (
   ID          INT  NOT NULL AUTO_INCREMENT,
   ID_user     INT  NOT NULL,
-  data-inicio DATE NOT NULL,
-  data-fim    DATE NOT NULL,
+  data-inicio DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  data-fim    DATE NULL     DEFAULT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE endereco
 (
-  ID      INT         NOT NULL AUTO_INCREMENT COMMENT 'começa em 1 para cada usuário',
-  ID_user INT         NOT NULL COMMENT 'AUTO-INCREMENT',
-  nome    VARCHAR(50) NULL     COMMENT 'nome do endereço (exemplo: casa, trabalho)',
+  ID      INT         NOT NULL AUTO_INCREMENT,
+  ID_user INT         NOT NULL,
+  nome    VARCHAR(50) NULL    ,
   CEP     VARCHAR(10) NOT NULL,
   PRIMARY KEY (ID)
 ) COMMENT 'tabela de ccade enderecos dos usuarios';
@@ -27,7 +27,7 @@ CREATE TABLE marca
 (
   ID     INT          NOT NULL AUTO_INCREMENT,
   nome   VARCHAR(100) NOT NULL,
-  imagem VARCHAR(255) NULL     COMMENT 'caminho do local da imagem salva',
+  imagem VARCHAR(255) NULL    ,
   PRIMARY KEY (ID)
 );
 
@@ -38,7 +38,7 @@ CREATE TABLE produto
   descricao  varchar(255) NULL    ,
   ID_marca   INT          NOT NULL,
   quantidade INT          NOT NULL DEFAULT 0,
-  preco_unit DECIMAL      NULL    ,
+  preco_unit DECIMAL      NOT NULL,
   PRIMARY KEY (ID)
 );
 
@@ -53,37 +53,22 @@ CREATE TABLE user
 (
   ID       INT          NOT NULL AUTO_INCREMENT,
   Username VARCHAR(150) NOT NULL,
-  Senha    VARCHAR(255) NOT NULL COMMENT 'HASH',
+  Senha    VARCHAR(255) NOT NULL,
   PRIMARY KEY (ID)
 ) COMMENT 'tabela de usuários';
 
 ALTER TABLE user
   ADD CONSTRAINT UQ_Username UNIQUE (Username);
 
-ALTER TABLE endereco
-  ADD CONSTRAINT FK_user_TO_endereco
-    FOREIGN KEY (ID_user)
-    REFERENCES user (ID);
-
-ALTER TABLE produto
-  ADD CONSTRAINT FK_marca_TO_produto
-    FOREIGN KEY (ID_marca)
-    REFERENCES marca (ID);
-
 ALTER TABLE compra
   ADD CONSTRAINT FK_user_TO_compra
     FOREIGN KEY (ID_user)
     REFERENCES user (ID);
 
-ALTER TABLE produto_compra
-  ADD CONSTRAINT FK_compra_TO_produto_compra
-    FOREIGN KEY (ID_compra)
-    REFERENCES compra (ID);
-
-ALTER TABLE produto_compra
-  ADD CONSTRAINT FK_produto_TO_produto_compra
-    FOREIGN KEY (ID_produto)
-    REFERENCES produto (ID);
+ALTER TABLE endereco
+  ADD CONSTRAINT FK_user_TO_endereco
+    FOREIGN KEY (ID_user)
+    REFERENCES user (ID);
 
 ALTER TABLE favoritos
   ADD CONSTRAINT FK_user_TO_favoritos
@@ -92,5 +77,20 @@ ALTER TABLE favoritos
 
 ALTER TABLE favoritos
   ADD CONSTRAINT FK_produto_TO_favoritos
+    FOREIGN KEY (ID_produto)
+    REFERENCES produto (ID);
+
+ALTER TABLE produto
+  ADD CONSTRAINT FK_marca_TO_produto
+    FOREIGN KEY (ID_marca)
+    REFERENCES marca (ID);
+
+ALTER TABLE produto_compra
+  ADD CONSTRAINT FK_compra_TO_produto_compra
+    FOREIGN KEY (ID_compra)
+    REFERENCES compra (ID);
+
+ALTER TABLE produto_compra
+  ADD CONSTRAINT FK_produto_TO_produto_compra
     FOREIGN KEY (ID_produto)
     REFERENCES produto (ID);
---
+++ Banco de dados/loja_simples - sql gerado por phpmyadmin.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08/08/2026 às 10:02
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `loja_simples`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `compra`
+--
+
+CREATE TABLE `compra` (
+  `ID` int(11) NOT NULL,
+  `ID_user` int(11) NOT NULL,
+  `data-inicio` date NOT NULL DEFAULT current_timestamp(),
+  `data-fim` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `ID` int(11) NOT NULL,
+  `ID_user` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `CEP` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `ID_user` int(11) NOT NULL,
+  `ID_produto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `marca`
+--
+
+CREATE TABLE `marca` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `imagem` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `quantidade` int(11) NOT NULL DEFAULT 0,
+  `preco_unitario` decimal(10,0) NOT NULL,
+  `ID_marca` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto_compra`
+--
+
+CREATE TABLE `produto_compra` (
+  `ID_compra` int(11) NOT NULL,
+  `ID_produto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_ID_USER_COMPRA` (`ID_user`);
+
+--
+-- Índices de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_ID_USER_ENDERECO` (`ID_user`);
+
+--
+-- Índices de tabela `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD KEY `FK_ID_PRODUTO_FAVORITOS` (`ID_produto`),
+  ADD KEY `FK_ID_USER_FAVORITOS` (`ID_user`);
+
+--
+-- Índices de tabela `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_ID_MARCA_PRODUTO` (`ID_marca`);
+
+--
+-- Índices de tabela `produto_compra`
+--
+ALTER TABLE `produto_compra`
+  ADD KEY `FK_ID_PRODUTO_P-C` (`ID_produto`),
+  ADD KEY `FK_ID_COMPRA_P-C` (`ID_compra`);
+
+--
+-- Índices de tabela `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `FK_ID_USER_COMPRA` FOREIGN KEY (`ID_user`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `endereco`
+--
+ALTER TABLE `endereco`
+  ADD CONSTRAINT `FK_ID_USER_ENDERECO` FOREIGN KEY (`ID_user`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `FK_ID_PRODUTO_FAVORITOS` FOREIGN KEY (`ID_produto`) REFERENCES `produto` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ID_USER_FAVORITOS` FOREIGN KEY (`ID_user`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `FK_ID_MARCA_PRODUTO` FOREIGN KEY (`ID_marca`) REFERENCES `marca` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `produto_compra`
+--
+ALTER TABLE `produto_compra`
+  ADD CONSTRAINT `FK_ID_COMPRA_P-C` FOREIGN KEY (`ID_compra`) REFERENCES `compra` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ID_PRODUTO_P-C` FOREIGN KEY (`ID_produto`) REFERENCES `produto` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
